bc60a7b7f9da0a5a2102ea712bee8da2aa567bd0
Gian Michele Innocenti <gian.michele.innocenti@cern.ch>
2021-02-07 00:45:04 +0100

    Add comparison plots

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -5,7 +5,7 @@ from ROOT import TH2F, TCanvas, TGraph, TLatex, gPad, TFile, TF1, TLegend
 import yaml
 from ROOT import gStyle
 from ROOT import gROOT
-from ROOT import TStyle
+from ROOT import TStyle, TLegendEntry
 
 def scale(hadron = "Omega_ccc", model = "SHMC_2021", collision = "PbPb", \
         brmode="central", energy = 2.76):
@@ -19,7 +19,7 @@ def scale(hadron = "Omega_ccc", model = "SHMC_2021", collision = "PbPb", \
     enhanc = 1
     eff = 0.01
     filename = "%s_%s_%s_%s" % (hadron, model, collision, brmode)
-    legendtext = '%s N_{ev}(%s) = %.1f B, BR=%.5f pc, eff=%.2f' \
+    legendtext = '%s N_{ev}(%s) = %.1f B, BR=%.5f%%, #varepsilon=%.2f' \
             % (model, collision, nevt/1e9, br*100, eff)
     scale_factor =br * enhanc * eff * nevt * yieldmid;
     return scale_factor, legendtext
@@ -42,19 +42,36 @@ def analysis(hadron="Omega_ccc"):
         colors = [1,2,4,5,6,7,9,11]
         latexname = "\Omega_{ccc}"
         useshape = "Omega_ccc"
+        ymin = 1e-4
+        ymax = 1e6
+
+    if hadron=="Omega_cc":
+        models = ["SHMC_2021", "SHMC_2021", "SHMC_2021", "SHMC_2021",
+                  "SHMC_2021", "SHMC_2021", "SHMC_2021"]
+        collisions = ["PbPb", "KrKr", "ArAr", "PbPb", "PbPb", "KrKr", "KrKr"]
+        brmode = ["central", "central", "central", "max", "min", "max", "min"]
+        colors = [1,2,4,5,6,7,9]
+        latexname = "\Omega_{cc}"
+        useshape = "Xi_cc"
+        ymin = 1e-2
+        ymax = 1e8
 
     if hadron=="Xi_cc":
-        models = ["SHMC_2021", "Stat_ChoLee_1", "Stat_ChoLee_2", "Catania"]
-        collisions = ["PbPb", "PbPb", "PbPb", "PbPb"]
-        brmode = ["central", "central", "central", "central"]
-        colors = [1,2,4,5]
+        models = ["SHMC_2021", "Stat_ChoLee_1", "Stat_ChoLee_2", "Catania",
+                  "SHMC_2021", "SHMC_2021", "SHMC_2021"]
+        collisions = ["PbPb", "PbPb", "PbPb", "PbPb", "KrKr", "KrKr", "KrKr"]
+        brmode = ["central", "central", "central", "central", "central", "min", "max"]
+        colors = [1,2,4,5,7,9,11]
         latexname = "\Xi_{cc}"
         useshape = "Xi_cc"
+        ymin = 1e-2
+        ymax = 1e8
 
     f = TFile("../Inputs/" + useshape +".root")
     histo_norm = f.Get("hpred_norm")
 
-    c1 = TCanvas( 'c1', 'A Simple Graph Example')
+    c1 = TCanvas("c1", "A Simple Graph Example",881,176,668,616);
+    gStyle.SetOptStat(0);
     c1.SetHighLightColor(2);
     c1.Range(-1.25,-4.625,11.25,11.625);
     c1.SetFillColor(0);
@@ -66,14 +83,14 @@ def analysis(hadron="Omega_ccc"):
     c1.cd()
     gPad.SetLogy()
 
-    hempty = TH2F("hempty", ";p_{T};#Delta N/#Delta p_{T}", 100, 0., 10., 100, 0.001, 1e10)
+    hempty = TH2F("hempty", ";p_{T};#Delta N/#Delta p_{T}", 100, 0., 10., 100, ymin, ymax)
     hempty.GetXaxis().SetTitle("p_{T}");
     hempty.GetXaxis().SetLabelFont(42);
     hempty.GetXaxis().SetTitleOffset(1);
     hempty.GetXaxis().SetTitleFont(42);
-    hempty.GetYaxis().SetTitle("#Delta N/#Delta p_{T}");
+    hempty.GetYaxis().SetTitle("#Delta N/#Delta p_{T} (GeV^{-1})");
     hempty.GetYaxis().SetLabelFont(42);
-    hempty.GetYaxis().SetTitleOffset(1.3);
+    hempty.GetYaxis().SetTitleOffset(1.35);
     hempty.GetYaxis().SetTitleFont(42);
     hempty.GetZaxis().SetLabelFont(42);
     hempty.GetZaxis().SetTitleOffset(1);
@@ -83,12 +100,13 @@ def analysis(hadron="Omega_ccc"):
     scalelist = [None]*len(models)
     textlist = [None]*len(models)
 
-    leg = TLegend(0.1293588,0.6101036,0.7457818,0.8743523,"","brNDC");
-    leg.SetBorderSize(1);
-    leg.SetLineColor(0);
-    leg.SetLineStyle(1);
-    leg.SetLineWidth(1);
-    leg.SetFillColor(0);
+    leg = TLegend(0.1471471,0.6108291,0.3018018,0.8747885,"","brNDC");
+    leg.SetBorderSize(1)
+    leg.SetLineColor(0)
+    leg.SetLineStyle(1)
+    leg.SetLineWidth(1)
+    leg.SetFillColor(0)
+    leg.SetTextSize(0.022)
     leg.SetFillStyle(1001);
 
     for icase in range(len(models)):
@@ -98,10 +116,12 @@ def analysis(hadron="Omega_ccc"):
             histolist[icase].SetBinContent(ibin+1, scalef*histolist[icase].GetBinContent(ibin+1))
         histolist[icase].SetLineColor(colors[icase])
         histolist[icase].SetMarkerColor(colors[icase])
+        histolist[icase].SetLineWidth(2)
         histolist[icase].Draw("same")
-        leg.AddEntry(histolist[icase], text, "lep")
+        leg.AddEntry(histolist[icase], text, "pF")
     leg.Draw()
     c1.SaveAs(hadron+"_results.pdf")
     c1.SaveAs(hadron+"_results.C")
 analysis("Omega_ccc")
+analysis("Omega_cc")
 analysis("Xi_cc")
--- analysis/read_predictions.py
+++ analysis/read_predictions.py
@@ -1,44 +1,42 @@
 from array import array
 import pandas as pd
 import numpy as np
-from ROOT import TH1F, TCanvas, TGraph, TLatex, gPad, TFile, TF1
+from ROOT import TH1F, TH2F, TCanvas, TGraph, TLatex, gPad, TFile, TF1
+from ROOT import gStyle
+from ROOT import gROOT
+from ROOT import TStyle, TLegendEntry
 
 def read_predictions(hadron = "Omega_ccc", collision = "PbPb"):
     nbins = 10
     minb = 0.
     maxb = 10.
     width = (maxb-minb)/float(nbins)
-
+    miny = 0
+    maxy = 0
     if hadron is "Omega_ccc":
-        br = 1.
-        enhanc = 1.
-        eff = 1.
         stringname = "Omega_ccc"
-        latexname = "\Omega_{ccc}"
+        latexname = "#Omega_{ccc}"
         filename = "Omega_ccc_LHC_PbPb_276TeV"
+        maxy = 1e-5
+        miny = 1e-9
     if hadron is "Xsi_cc":
-        br = 1.
-        enhanc = 1.
-        eff = 1.
         stringname = "Xi_cc"
-        latexname = "\Xi_{cc}"
+        latexname = "#Xi_{cc}"
         filename = "Xsi_cc_LHC_PbPb_276TeV"
+        maxy = 1e-2
+        miny = 1e-6
     if hadron is "Lc":
-        br = 1.
-        enhanc = 1.
-        eff = 1.
         stringname = "Lc"
-        latexname = "\Lambda_{c}"
+        latexname = "#Lambda_{c}"
         filename = "Lc_LHC_PbPb_276TeV"
+        maxy = 1e2
+        miny = 1e-4
 
     if collision is "PbPb":
         nevt = 1.
         energy = 2.76
 
-    legendtext = '%s %.2f TeV, N^{MB}_{ev} = %.1f B, BR=%.3f percent \
-       efficiency=%.3f, %d x w.r.t. coalescence' % (collision, energy, nevt/1e9, br*100, eff, enhanc)
-    scale_factor =br * enhanc * eff * nevt;
-
+    legendtext = '%s %.2f TeV, arXiv.1907.12786 (Coal.2)' % (collision, energy)
     df = pd.read_csv("../Inputs/" + filename+".csv")
     pt = df["pt"]
     cross = df["cross"]
@@ -46,33 +44,16 @@ def read_predictions(hadron = "Omega_ccc", collision = "PbPb"):
 
     pt_val = array('f', pt)
     cross_val = array('f', cross)
-    cross_val_ptscaled = array('f',[2*3.14*scale_factor*a*b for a,b in zip(pt_val, cross_val)])
+    cross_val_ptscaled = array('f',[2*3.14*a*b for a,b in zip(pt_val, cross_val)])
     n = 25
     gr = TGraph( n, pt_val, cross_val_ptscaled)
-    gr.SetLineColor(2)
+    gr.SetLineColor(1)
     gr.SetLineWidth(4)
-    gr.SetMarkerColor(4)
+    gr.SetMarkerColor(1)
     gr.SetMarkerStyle(21)
     gr.SetTitle('')
     gr.GetXaxis().SetTitle('p_{T} (GeV)')
     gr.GetYaxis().SetTitle('dN/dp_{T} (%s) (GeV^{-1})' % latexname)
-
-    c1 = TCanvas( 'c1', 'A Simple Graph Example')
-    c1.SetCanvasSize(1500, 1000)
-    c1.cd()
-    gPad.SetLogy()
-    hempty = TH1F("hempty", "hempty", 100, 0., 1033.)
-    hempty.Draw()
-    gr.Draw('ACPsame')
-    tex = TLatex()
-    tex.SetNDC()
-    tex.SetTextFont(62)
-    tex.SetTextColor(1)
-    tex.SetTextSize( 0.03)
-    tex.SetTextAlign(12)
-    tex.DrawLatex( 0.12, 0.85, legendtext)
-    tex.Draw()
-    c1.SaveAs("../Inputs/" + filename+".pdf")
 
     histo = TH1F("hpred", ";p_{T}; #Delta N/#Delta p_{T}", nbins, minb, maxb)
     norm = 0.
@@ -87,7 +68,47 @@ def read_predictions(hadron = "Omega_ccc", collision = "PbPb"):
     for i in range(nbins-1):
       histo_norm.SetBinContent(i+1,histo.GetBinContent(i+1)/norm)
     histo_norm.Write()
-    print(norm, latexname)
+
+    c1 = TCanvas("c1", "A Simple Graph Example",881,176,668,616);
+    gStyle.SetOptStat(0);
+    c1.SetHighLightColor(2);
+    c1.Range(-1.25,-4.625,11.25,11.625);
+    c1.SetFillColor(0);
+    c1.SetBorderMode(0);
+    c1.SetBorderSize(2);
+    c1.SetLogy();
+    c1.SetFrameBorderMode(0);
+    c1.SetFrameBorderMode(0);
+    c1.cd()
+    gPad.SetLogy()
+    hempty = TH2F("hempty", ";p_{T};dN/d p_{T}", 100, 0., 8., 100, miny, maxy)
+    hempty.GetYaxis().SetTitle('dN/dp_{T} (%s) (GeV^{-1})' % latexname)
+    hempty.GetXaxis().SetTitle("p_{T}");
+    hempty.GetXaxis().SetLabelFont(42);
+    hempty.GetXaxis().SetTitleOffset(1);
+    hempty.GetXaxis().SetTitleFont(42);
+    hempty.GetYaxis().SetLabelFont(42);
+    hempty.GetYaxis().SetTitleOffset(1.35);
+    hempty.GetYaxis().SetTitleFont(42);
+    hempty.GetZaxis().SetLabelFont(42);
+    hempty.GetZaxis().SetTitleOffset(1);
+    hempty.GetZaxis().SetTitleFont(42);
+    hempty.Draw()
+    gr.Draw('Psame')
+    histo.SetLineColor(1)
+    histo.SetLineWidth(2)
+    histo.Draw("same")
+    tex = TLatex()
+    tex.SetNDC()
+    tex.SetTextFont(40)
+    tex.SetTextColor(1)
+    tex.SetTextSize( 0.03)
+    tex.SetTextAlign(12)
+    tex.DrawLatex( 0.12, 0.85, legendtext + ", dNdy=%.8f" % norm)
+    #tex.Draw()
+    c1.SaveAs("../Inputs/" + filename+".pdf")
+    c1.SaveAs("../Inputs/" + filename+".C")
+
 read_predictions("Omega_ccc", "PbPb")
 read_predictions("Xsi_cc", "PbPb")
 read_predictions("Lc", "PbPb")
