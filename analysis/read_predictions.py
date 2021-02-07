f49b5cd40c49426575d49896d25260b940192da8
Gian Michele Innocenti <gian.michele.innocenti@cern.ch>
2021-02-07 12:12:30 +0100

    pylint

--- analysis/read_predictions.py
+++ analysis/read_predictions.py
@@ -1,13 +1,16 @@
+#pylint: disable=too-many-locals,too-many-statements, missing-docstring, pointless-string-statement
 from array import array
-import pandas as pd
-import numpy as np
-from ROOT import TH1F, TH2F, TCanvas, TGraph, TLatex, gPad, TFile, TF1
 import yaml
-from ROOT import gStyle
-from ROOT import gROOT
-from ROOT import TStyle, TLegendEntry
+import pandas as pd #pylint: disable=import-error
+# pylint: disable=import-error, no-name-in-module, unused-import
+from ROOT import TH1F, TH2F, TCanvas, TGraph, TLatex, gPad, TFile, TF1
+from ROOT import gStyle, gROOT, TStyle, TLegendEntry
 
-def read_predictions(hadron = "Omega_ccc", collision = "PbPb"):
+"""
+read predictions from arXiv.1907.12786
+"""
+
+def read_predictions(hadron="Omega_ccc", collision="PbPb"):
     do2pipnorm = 1
     with open(r'prediction.yaml') as fileparam:
         param = yaml.load(fileparam, Loader=yaml.FullLoader)
@@ -22,68 +25,67 @@ def read_predictions(hadron = "Omega_ccc", collision = "PbPb"):
     energy = 2.76
 
     legendtext = '%s %.2f TeV, arXiv.1907.12786 (Coal.2)' % (collision, energy)
-    df = pd.read_csv("../Inputs/" + filename+".csv")
-    pt = df["pt"]
-    cross = df["cross"]
-    hist = TH1F('hist', 'hist', 100, 0, 1)
+    dfpred = pd.read_csv("../Inputs/" + filename+".csv")
+    ptcand = dfpred["pt"]
+    cross = dfpred["cross"]
 
-    pt_val = array('f', pt)
+    ptcand_val = array('f', ptcand)
     cross_val = array('f', cross)
     if do2pipnorm == 1:
-        cross_val_ptscaled = array('f',[2*3.14*a*b for a,b in zip(pt_val, cross_val)])
+        cross_val_ptscaled = array('f', [2*3.14*a*b for a, b in zip(ptcand_val, cross_val)])
     if do2pipnorm == 0:
         cross_val_ptscaled = cross_val
-    n = 25
-    gr = TGraph( n, pt_val, cross_val_ptscaled)
-    gr.SetLineColor(1)
-    gr.SetLineWidth(4)
-    gr.SetMarkerColor(1)
-    gr.SetMarkerStyle(21)
-    gr.SetTitle('')
-    gr.GetXaxis().SetTitle('p_{T} (GeV)')
-    gr.GetYaxis().SetTitle('dN/dp_{T} (%s) (GeV^{-1})' % latexname)
+    npoints = 25
+    grpred = TGraph(npoints, ptcand_val, cross_val_ptscaled)
+    grpred.SetLineColor(1)
+    grpred.SetLineWidth(4)
+    grpred.SetMarkerColor(1)
+    grpred.SetMarkerStyle(21)
+    grpred.SetTitle('')
+    grpred.GetXaxis().SetTitle('p_{T} (GeV)')
+    grpred.GetYaxis().SetTitle('dN/dp_{T} (%s) (GeV^{-1})' % latexname)
 
     histo = TH1F("hpred", ";p_{T}; #Delta N/#Delta p_{T}", nbins, minb, maxb)
     norm = 0.
     for i in range(nbins-1):
-      histo.SetBinContent(i+1,gr.Eval(i*width+width/2.))
-      print(i+1, i*width+width/2., gr.Eval(i*width+width/2.))
-      norm = norm + width*gr.Eval(i*width+width/2.)
-    f = TFile("../Inputs/" + hadron+".root", "recreate")
-    gr.Write()
+        histo.SetBinContent(i+1, grpred.Eval(i*width+width/2.))
+        print(i+1, i*width+width/2., grpred.Eval(i*width+width/2.))
+        norm = norm + width*grpred.Eval(i*width+width/2.)
+    fout = TFile("../Inputs/" + hadron+".root", "recreate")
+    fout.cd()
+    grpred.Write()
     histo.Write()
     histo_norm = TH1F("hpred_norm", ";p_{T}; #Delta N/#Delta p_{T}", nbins, minb, maxb)
     for i in range(nbins-1):
-      histo_norm.SetBinContent(i+1,histo.GetBinContent(i+1)/norm)
+        histo_norm.SetBinContent(i+1, histo.GetBinContent(i+1)/norm)
     histo_norm.Write()
 
-    c1 = TCanvas("c1", "A Simple Graph Example",881,176,668,616);
-    gStyle.SetOptStat(0);
-    c1.SetHighLightColor(2);
-    c1.Range(-1.25,-4.625,11.25,11.625);
-    c1.SetFillColor(0);
-    c1.SetBorderMode(0);
-    c1.SetBorderSize(2);
-    c1.SetLogy();
-    c1.SetFrameBorderMode(0);
-    c1.SetFrameBorderMode(0);
-    c1.cd()
+    cpred = TCanvas("cpred", "A Simple Graph Example", 881, 176, 668, 616)
+    gStyle.SetOptStat(0)
+    cpred.SetHighLightColor(2)
+    cpred.Range(-1.25, -4.625, 11.25, 11.625)
+    cpred.SetFillColor(0)
+    cpred.SetBorderMode(0)
+    cpred.SetBorderSize(2)
+    cpred.SetLogy()
+    cpred.SetFrameBorderMode(0)
+    cpred.SetFrameBorderMode(0)
+    cpred.cd()
     gPad.SetLogy()
-    print(miny,maxy)
     hempty = TH2F("hempty", ";p_{T};dN/d p_{T}", 100, 0., 8., 100, miny, maxy)
     hempty.GetYaxis().SetTitle('dN/dp_{T} (%s) (GeV^{-1})' % latexname)
-    hempty.GetXaxis().SetTitle("p_{T}");
-    hempty.GetXaxis().SetLabelFont(42);
-    hempty.GetXaxis().SetTitleOffset(1);
-    hempty.GetXaxis().SetTitleFont(42);
-    hempty.GetYaxis().SetLabelFont(42);
-    hempty.GetYaxis().SetTitleOffset(1.35);
-    hempty.GetYaxis().SetTitleFont(42);
-    hempty.GetZaxis().SetLabelFont(42);
-    hempty.GetZaxis().SetTitleOffset(1);
-    hempty.GetZaxis().SetTitleFont(42);
+    hempty.GetXaxis().SetTitle("p_{T}")
+    hempty.GetXaxis().SetLabelFont(42)
+    hempty.GetXaxis().SetTitleOffset(1)
+    hempty.GetXaxis().SetTitleFont(42)
+    hempty.GetYaxis().SetLabelFont(42)
+    hempty.GetYaxis().SetTitleOffset(1.35)
+    hempty.GetYaxis().SetTitleFont(42)
+    hempty.GetZaxis().SetLabelFont(42)
+    hempty.GetZaxis().SetTitleOffset(1)
+    hempty.GetZaxis().SetTitleFont(42)
     hempty.Draw()
-    gr.Draw('Psame')
+    grpred.Draw('Psame')
     histo.SetLineColor(1)
     histo.SetLineWidth(2)
     histo.Draw("same")
@@ -91,12 +93,11 @@ def read_predictions(hadron = "Omega_ccc", collision = "PbPb"):
     tex.SetNDC()
     tex.SetTextFont(40)
     tex.SetTextColor(1)
-    tex.SetTextSize( 0.03)
+    tex.SetTextSize(0.03)
     tex.SetTextAlign(12)
-    tex.DrawLatex( 0.12, 0.85, legendtext + ", dNdy=%.8f" % norm)
+    tex.DrawLatex(0.12, 0.85, legendtext + ", dNdy=%.8f" % norm)
     #tex.Draw()
-    c1.SaveAs("../Inputs/" + filename+".pdf")
-    c1.SaveAs("../Inputs/" + filename+".C")
+    cpred.SaveAs("../Inputs/" + filename+".pdf")
 
 read_predictions("Omega_ccc", "PbPb")
 read_predictions("Omega_cc", "PbPb")
