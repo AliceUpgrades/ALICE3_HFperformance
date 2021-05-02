844b49512e89fce1be7de3a2bd2b5e1f3dbbf9be
Gian Michele Innocenti <ginnocen@alicecerno2.cern.ch>
2021-05-02 17:06:05 +0200

    Formatting

--- pythia/examplehadron.cc
+++ pythia/examplehadron.cc
@@ -1,4 +1,4 @@
-// This is a simple macro to generate pythia predictions 
+// This is a simple macro to generate pythia predictions
 
 #include <algorithm>
 #include <iostream>
@@ -27,8 +27,8 @@
 #include "TRandom3.h"
 #include "TH1D.h"
 #include "fastjet/PseudoJet.hh"
-#include "fastjet/ClusterSequence.hh"
-#include "fastjet/ClusterSequenceArea.hh"  
+//#include "fastjet/ClusterSequence.hh"
+//#include "fastjet/ClusterSequenceArea.hh"
 #include <ctime>
 #include <iostream> // needed for io
 #include <cstdio>   // needed for io
@@ -42,114 +42,115 @@
 using namespace Pythia8;
 
 int main(int argc, char* argv[]) {
-  (void)argc;
-  std::string mycase = argv[1];
-  int cislo = -1;                 //unique number for each file
-  cislo = atoi(argv[2]);
-  // number of parallel jobs to be run. Be aware that each single file
-  // will be normalized by this number to make sure that the merged output
-  // file has the proper normalization!
-  int n_jobs = -1;
-  n_jobs = atoi(argv[3]);
+    (void)argc;
+    std::string mycase = argv[1];
+    int cislo = -1;                 //unique number for each file
+    cislo = atoi(argv[2]);
+    // number of parallel jobs to be run. Be aware that each single file
+    // will be normalized by this number to make sure that the merged output
+    // file has the proper normalization!
+    int n_jobs = -1;
+    n_jobs = atoi(argv[3]);
 
-  YAML::Node node = YAML::LoadFile("config.yaml");
-  YAML::Node nodecase = node[mycase.data()];
+    YAML::Node node = YAML::LoadFile("config.yaml");
+    YAML::Node nodecase = node[mycase.data()];
 
-  const std::string myhadronname = nodecase["myhadronname"].as<std::string>();
-  const std::string myhadronlatex = nodecase["myhadronlatex"].as<std::string>();
-  int pdgparticle = nodecase["pdgparticle"].as<int>();
-  float correction = nodecase["correction"].as<float>();
-  int maxnevents = nodecase["maxneventsperjob"].as<int>();
-  int tune = nodecase["tune"].as<int>();
-  int beamidA = nodecase["beamidA"].as<int>();
-  int beamidB = nodecase["beamidB"].as<int>();
-  float eCM = nodecase["eCM"].as<float>();
-  const std::string pythiamode = nodecase["pythiamode"].as<std::string>();
-  const std::string outputfile = nodecase["outputfile"].as<std::string>();
-  double nptbins = nodecase["nptbins"].as<int>();
-  double ptmin = nodecase["ptmin"].as<float>();
-  double ptmax = nodecase["ptmax"].as<float>();
-  double ymin = nodecase["ymin"].as<float>();
-  double ymax = nodecase["ymax"].as<float>();
-  const std::string extramode = nodecase["extramode"].as<std::string>();
+    const std::string myhadronname = nodecase["myhadronname"].as<std::string>();
+    const std::string myhadronlatex = nodecase["myhadronlatex"].as<std::string>();
+    int pdgparticle = nodecase["pdgparticle"].as<int>();
+    float correction = nodecase["correction"].as<float>();
+    int maxnevents = nodecase["maxneventsperjob"].as<int>();
+    int tune = nodecase["tune"].as<int>();
+    int beamidA = nodecase["beamidA"].as<int>();
+    int beamidB = nodecase["beamidB"].as<int>();
+    float eCM = nodecase["eCM"].as<float>();
+    const std::string pythiamode = nodecase["pythiamode"].as<std::string>();
+    const std::string outputfile = nodecase["outputfile"].as<std::string>();
+    double nptbins = nodecase["nptbins"].as<int>();
+    double ptmin = nodecase["ptmin"].as<float>();
+    double ptmax = nodecase["ptmax"].as<float>();
+    double ymin = nodecase["ymin"].as<float>();
+    double ymax = nodecase["ymax"].as<float>();
+    const std::string extramode = nodecase["extramode"].as<std::string>();
 
-  //END OF CONFIGURATION
-  
+    //END OF CONFIGURATION
 
-  // Generator. Process selection. LHC initialization. Histogram.
-  Pythia pythia;
-  pythia.readString(Form("%s", pythiamode.data()));
-  pythia.readString(Form("Main:numberOfEvents = %d", maxnevents));
-  pythia.readString("Next:numberShowEvent = 0");
-  pythia.readString(Form("Tune:pp = %d", tune));
-  pythia.readString(Form("Beams:idA = %d", beamidA));
-  pythia.readString(Form("Beams:idB = %d", beamidB));
-  pythia.readString(Form("Beams:eCM = %f", eCM));
 
-  pythia.readString("Random:setSeed = on");
-  pythia.readString(Form("Random:seed = %d",cislo));
+    // Generator. Process selection. LHC initialization. Histogram.
+    Pythia pythia;
+    pythia.readString(Form("%s", pythiamode.data()));
+    pythia.readString(Form("Main:numberOfEvents = %d", maxnevents));
+    pythia.readString("Next:numberShowEvent = 0");
+    pythia.readString(Form("Tune:pp = %d", tune));
+    pythia.readString(Form("Beams:idA = %d", beamidA));
+    pythia.readString(Form("Beams:idB = %d", beamidB));
+    pythia.readString(Form("Beams:eCM = %f", eCM));
 
-  if (extramode=="mode2") {
-      pythia.readString("ColourReconnection:mode = 1");
-      pythia.readString("ColourReconnection:allowDoubleJunRem = off");
-      pythia.readString("ColourReconnection:m0 = 0.3");
-      pythia.readString("ColourReconnection:allowJunctions = on");
-      pythia.readString("ColourReconnection:junctionCorrection = 1.20");
-      pythia.readString("ColourReconnection:timeDilationMode = 2");
-      pythia.readString("ColourReconnection:timeDilationPar = 0.18");
-      pythia.readString("StringPT:sigma = 0.335");
-      pythia.readString("StringZ:aLund = 0.36");
-      pythia.readString("StringZ:bLund = 0.56");
-      pythia.readString("StringFlav:probQQtoQ = 0.078");
-      pythia.readString("StringFlav:ProbStoUD = 0.2");
-      pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-      pythia.readString("MultiPartonInteractions:pT0Ref = 2.15");
-      pythia.readString("BeamRemnants:remnantMode = 1");
-      pythia.readString("BeamRemnants:saturation =5");
-  }
-  pythia.init();
+    pythia.readString("Random:setSeed = on");
+    pythia.readString(Form("Random:seed = %d",cislo));
 
-  TFile *fout = new TFile(outputfile.data(), "recreate");
- 
-  fout->cd();
-  TH1F*hparticlept = new TH1F("hchargedparticles_pt", ";p_{T};charged particle dN/dp_{T}", nptbins, ptmin, ptmax);
-  TH1F*hptyields_unnorm = new TH1F(Form("h%syieldsvspt_unnorm", myhadronname.data()), ";p_{T} (GeV);unnormalized yield (particle+anti)", nptbins, ptmin, ptmax);
-  TH1F*hptcross = new TH1F(Form("h%scrossvspt", myhadronname.data()), Form(";p_{T} (GeV);%s d#sigma^{PYTHIA}/dp_{T} (#mu b/GeV)", myhadronlatex.data()), nptbins, ptmin, ptmax);
-
-  // Begin event loop. Generate event. Skip if error. List first one.
-  int nmyhadron = 0;
-  for (int iEvent = 0; iEvent < maxnevents; ++iEvent) {
-
-    pythia.next();
-   
-    for (int i = 0; i < pythia.event.size(); ++i){
-      if(pythia.event[i].pT()<0 || pythia.event[i].pT()>1.e+5) continue;
-      if(pythia.event[i].y()<ymin || pythia.event[i].y()>ymax) continue;
-
-	hparticlept->Fill(pythia.event[i].pT());
-	if(pythia.event[i].idAbs()==pdgparticle) {++nmyhadron;
-		hptyields_unnorm->Fill(pythia.event[i].pT());
-	}
-       // End of event loop. Statistics. Histogram. Done.
+    if (extramode=="mode2") {
+        pythia.readString("ColourReconnection:mode = 1");
+        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
+        pythia.readString("ColourReconnection:m0 = 0.3");
+        pythia.readString("ColourReconnection:allowJunctions = on");
+        pythia.readString("ColourReconnection:junctionCorrection = 1.20");
+        pythia.readString("ColourReconnection:timeDilationMode = 2");
+        pythia.readString("ColourReconnection:timeDilationPar = 0.18");
+        pythia.readString("StringPT:sigma = 0.335");
+        pythia.readString("StringZ:aLund = 0.36");
+        pythia.readString("StringZ:bLund = 0.56");
+        pythia.readString("StringFlav:probQQtoQ = 0.078");
+        pythia.readString("StringFlav:ProbStoUD = 0.2");
+        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
+        pythia.readString("MultiPartonInteractions:pT0Ref = 2.15");
+        pythia.readString("BeamRemnants:remnantMode = 1");
+        pythia.readString("BeamRemnants:saturation =5");
     }
+    pythia.init();
+
+    TFile *fout = new TFile(outputfile.data(), "recreate");
+
+    fout->cd();
+    TH1F*hparticlept = new TH1F("hchargedparticles_pt", ";p_{T};charged particle dN/dp_{T}", nptbins, ptmin, ptmax);
+    TH1F*hptyields_unnorm = new TH1F(Form("h%syieldsvspt_unnorm", myhadronname.data()), ";p_{T} (GeV);unnormalized yield (particle+anti)", nptbins, ptmin, ptmax);
+    TH1F*hptcross = new TH1F(Form("h%scrossvspt", myhadronname.data()), Form(";p_{T} (GeV);%s d#sigma^{PYTHIA}/dp_{T} (#mu b/GeV)", myhadronlatex.data()), nptbins, ptmin, ptmax);
+
+    // Begin event loop. Generate event. Skip if error. List first one.
+    int nmyhadron = 0;
+    for (int iEvent = 0; iEvent < maxnevents; ++iEvent) {
+
+        pythia.next();
+
+        for (int i = 0; i < pythia.event.size(); ++i) {
+            if(pythia.event[i].pT()<0 || pythia.event[i].pT()>1.e+5) continue;
+            if(pythia.event[i].y()<ymin || pythia.event[i].y()>ymax) continue;
+
+            hparticlept->Fill(pythia.event[i].pT());
+            if(pythia.event[i].idAbs()==pdgparticle) {
+                ++nmyhadron;
+                hptyields_unnorm->Fill(pythia.event[i].pT());
+            }
+            // End of event loop. Statistics. Histogram. Done.
+        }
     }
-     pythia.stat();
-     int nbinspT=hptcross->GetNbinsX();
-     double norm_fact = pythia.info.sigmaGen()*1000/(2*n_jobs*pythia.info.nAccepted())*correction;
-     printf("norm fact %f\n", norm_fact);
-     double contentmyhadron[nbinspT], binwidthmyhadron[nbinspT];
-   
-     for(int ibin=0; ibin < nbinspT; ibin++){
-            contentmyhadron[ibin] = hptyields_unnorm->GetBinContent(ibin);
-	    binwidthmyhadron[ibin]= hptyields_unnorm->GetBinWidth(ibin);
-	    printf("bin %d, Content %f, binwidth %f\n",ibin, contentmyhadron[ibin], binwidthmyhadron[ibin]);
-	    hptcross ->SetBinContent(ibin, contentmyhadron[ibin]*norm_fact/binwidthmyhadron[ibin]);
-     }
-  printf("nAccepted %ld, nTried %ld\n", pythia.info.nAccepted(), pythia.info.nTried());
-  printf("pythia.info.sigmaGen() %f\n", pythia.info.sigmaGen()); 
-  printf("N myhadron %d", nmyhadron);
-  hptcross -> Write();
-  hptyields_unnorm->Write();
-  fout->Write();
-  return 0;
+    pythia.stat();
+    int nbinspT=hptcross->GetNbinsX();
+    double norm_fact = correction*pythia.info.sigmaGen()*1000/(2*n_jobs*pythia.info.nAccepted());
+    printf("norm fact %f\n", norm_fact);
+    double contentmyhadron[nbinspT], binwidthmyhadron[nbinspT];
+
+    for(int ibin=0; ibin < nbinspT; ibin++) {
+        contentmyhadron[ibin] = hptyields_unnorm->GetBinContent(ibin);
+        binwidthmyhadron[ibin]= hptyields_unnorm->GetBinWidth(ibin);
+        printf("bin %d, Content %f, binwidth %f\n",ibin, contentmyhadron[ibin], binwidthmyhadron[ibin]);
+        hptcross ->SetBinContent(ibin, contentmyhadron[ibin]*norm_fact/binwidthmyhadron[ibin]);
+    }
+    printf("nAccepted %ld, nTried %ld\n", pythia.info.nAccepted(), pythia.info.nTried());
+    printf("pythia.info.sigmaGen() %f\n", pythia.info.sigmaGen());
+    printf("N myhadron %d", nmyhadron);
+    hptcross -> Write();
+    hptyields_unnorm->Write();
+    fout->Write();
+    return 0;
 }
